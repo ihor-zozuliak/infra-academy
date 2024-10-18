0731ffb88906075f1641524810d7aa0d9cc8dc2b
Ihor Zozuliak <ihor.zozuliak@avenga.com>
2024-10-19 01:33:51 +0300

    update vpc module

--- modules/infra/vpc/main.tf
+++ modules/infra/vpc/main.tf
@@ -1,32 +1,37 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr
-  tags = {
+# Module AWS VPC
+# VPC resource
+resource "aws_vpc" "vpc" {
+  cidr_block                           = var.vpc_cidr
+  instance_tenancy                     = var.tenancy
+  enable_dns_support                   = var.dns_support
+  enable_dns_hostnames                 = var.dns_hostnames
+  enable_network_address_usage_metrics = var.tenancy
     Environment = var.environment
     Owner       = var.owner
     terraform   = "true"
   }
-  enable_dns_support   = true
-  enable_dns_hostnames = true
 }
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
+
+resource "aws_subnet" "vpc_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.subnet_cidr
   availability_zone       = var.az
   map_public_ip_on_launch = true
+  ipv6_native =
 }
-resource "aws_internet_gateway" "my_ig" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_internet_gateway" "vpc_ig" {
+  vpc_id = aws_vpc.vpc.id
   tags = {
     Environment = var.environment
     Owner       = var.owner
     terraform   = "true"
   }
 }
-resource "aws_route_table" "my_rt" {
+resource "aws_route_table" "vpc_rt" {
   vpc_id = aws_vpc.my_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_ig.id
+    gateway_id = aws_internet_gateway.vpc_ig.id
   }
   tags = {
     Environment = var.environment
--- modules/infra/vpc/variables.tf
+++ modules/infra/vpc/variables.tf
@@ -1,4 +1,27 @@
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+  description = "The IPv4 CIDR block for the VPC"
+  type        = string
+}
+variable "dns_support" {
+  description = "A boolean flag to enable/disable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+variable "dns_hostnames" {
+  description = "A boolean flag to enable/disable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+variable "tenancy" {
+  description = "A boolean flag to enable/disable DNS hostnames in the VPC"
+  type        = string
+  default     = "default" # "default" or "dedicated"
+}
+variable "network_metrics" {
+  description = "Indicates whether Network Address Usage metrics are enabled for your VPC"
+  type        = bool
+  default     = false
+}
 variable "az" {}
 variable "subnet_cidr" {}
 variable "environment" {}
